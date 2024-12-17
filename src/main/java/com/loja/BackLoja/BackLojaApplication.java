82543e6e7389e68f33fcdb5f4f81404e475fc127
Paulo Rafael <paulorafaelcarvalhorodrigues@gmail.com>
2024-12-17 18:03:01 -0300

    Criação do projeto

---
+++ src/main/java/com/loja/BackLoja/BackLojaApplication.java
@@ -0,0 +1,13 @@
+package com.loja.BackLoja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loja/BackLoja/BackLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loja.BackLoja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
